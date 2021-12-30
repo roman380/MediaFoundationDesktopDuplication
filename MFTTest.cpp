7ec6871e960db65a5133ad0ecd23477612df2bdd
Admin <Admin@LAPTOP-I9FK3CRB>
2021-12-30 17:32:54 -0600

    Many updates

--- MFTTest.cpp
+++ MFTTest.cpp
@@ -31,8 +31,6 @@ CComPtr<IMFVideoSampleAllocatorEx> allocator;
 
 CComPtr<IMFAttributes> transformAttrs;
 CComQIPtr<IMFMediaEventGenerator> eventGen;
-DWORD inputStreamID;
-DWORD outputStreamID;
 
 HRESULT InitMF()
 {
@@ -72,12 +70,12 @@ HRESULT GetEncoder(IN const CComPtr<ID3D11Device>& device, OUT CComPtr<IMFTransf
 	UINT32 activateCount = 0;
 
 	// Input & output types
-	//MFT_REGISTER_TYPE_INFO inInfo = { MFMediaType_Video, MFVideoFormat_NV12 };
+	MFT_REGISTER_TYPE_INFO inInfo = { MFMediaType_Video, MFVideoFormat_NV12 };
 	MFT_REGISTER_TYPE_INFO outInfo = { MFMediaType_Video, MFVideoFormat_H264 };
 
 	// Query for the adapter LUID to get a matching encoder for the device.
 	CComQIPtr<IDXGIDevice> dxgiDevice(device);
-	//CHECK(dxgiDevice);
+
 	CComPtr<IDXGIAdapter> adapter;
 	if (FAILED(hr = dxgiDevice->GetAdapter(&adapter)))
 		return hr;
@@ -93,11 +91,9 @@ HRESULT GetEncoder(IN const CComPtr<ID3D11Device>& device, OUT CComPtr<IMFTransf
 	if (FAILED(hr = enumAttrs->SetBlob(MFT_ENUM_ADAPTER_LUID, (BYTE*)&adapterDesc.AdapterLuid, sizeof(LUID))))
 		return hr;
 
-	if (FAILED(hr = MFTEnum2(MFT_CATEGORY_VIDEO_ENCODER, MFT_ENUM_FLAG_HARDWARE | MFT_ENUM_FLAG_SORTANDFILTER, nullptr, &outInfo,
+	if (FAILED(hr = MFTEnum2(MFT_CATEGORY_VIDEO_ENCODER, MFT_ENUM_FLAG_HARDWARE | MFT_ENUM_FLAG_SORTANDFILTER, &inInfo, &outInfo,
 		enumAttrs, &activateRaw, &activateCount)))
 		return hr;
-
-	//CHECK(activateCount != 0);
 
 	// Choose the first returned encoder
 	activate = activateRaw[0];
@@ -117,6 +113,58 @@ HRESULT GetEncoder(IN const CComPtr<ID3D11Device>& device, OUT CComPtr<IMFTransf
 	std::cout << "- GetEncoder() Found " << activateCount << " encoders" << std::endl;
 
 	return hr;
+}
+
+HRESULT ConfigureEncoder(IN CComPtr<IMFTransform>& transform, CComPtr<IMFDXGIDeviceManager>& deviceManager, DWORD inputStreamID, 
+	DWORD outputStreamID
+)
+{
+	HRESULT hr = S_OK;
+	// Sets or clears the Direct3D Device Manager for DirectX Video Accereration (DXVA).
+	if (FAILED(hr = transform->ProcessMessage(MFT_MESSAGE_SET_D3D_MANAGER, reinterpret_cast<ULONG_PTR>(deviceManager.p))))
+		return hr;
+
+	// Set output type
+	CComPtr<IMFMediaType> outputType;
+	if (FAILED(hr = MFCreateMediaType(&outputType)))
+		return hr;
+
+	if (FAILED(hr = outputType->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video)))
+		return hr;
+	if (FAILED(hr = outputType->SetGUID(MF_MT_SUBTYPE, MFVideoFormat_H264)))
+		return hr;
+	if (FAILED(hr = outputType->SetUINT32(MF_MT_AVG_BITRATE, 30000000)))
+		return hr;
+	if (FAILED(hr = MFSetAttributeSize(outputType, MF_MT_FRAME_SIZE, ENCODE_WIDTH, ENCODE_HEIGHT)))
+		return hr;
+	if (FAILED(hr = MFSetAttributeRatio(outputType, MF_MT_FRAME_RATE, 60, 1)))
+		return hr;
+	if (FAILED(hr = outputType->SetUINT32(MF_MT_INTERLACE_MODE, 2)))
+		return hr;
+	if (FAILED(hr = outputType->SetUINT32(MF_MT_ALL_SAMPLES_INDEPENDENT, true)))
+		return hr;
+
+	if (FAILED(hr = transform->SetOutputType(outputStreamID, outputType, 0)))
+		return hr;
+
+	// Set input type
+	CComPtr<IMFMediaType> inputType;
+	if (FAILED(hr = transform->GetInputAvailableType(inputStreamID, 0, &inputType)))
+		return hr;
+
+	if (FAILED(hr = inputType->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video)))
+		return hr;
+	if (FAILED(hr = inputType->SetGUID(MF_MT_SUBTYPE, MFVideoFormat_NV12)))
+		return hr;
+	if (FAILED(hr = MFSetAttributeSize(inputType, MF_MT_FRAME_SIZE, ENCODE_WIDTH, ENCODE_HEIGHT)))
+		return hr;
+	if (FAILED(hr = MFSetAttributeRatio(inputType, MF_MT_FRAME_RATE, 60, 1)))
+		return hr;
+
+	if (FAILED(hr = transform->SetInputType(inputStreamID, inputType, 0)))
+		return hr;
+
+	std::cout << "- Set encoder configuration" << std::endl;
 }
 
 int main()
@@ -160,6 +208,21 @@ int main()
 	if (FAILED(hr = transformAttrs->SetUINT32(MF_TRANSFORM_ASYNC_UNLOCK, true)))
 		return hr;
 
+	// Get Stream IDs
+	DWORD inputStreamID, outputStreamID;
+	hr = transform->GetStreamIDs(1, &inputStreamID, 1, &outputStreamID);
+	if (hr == E_NOTIMPL) // Doesn't mean failed, see remarks
+	{				     // https://docs.microsoft.com/en-us/windows/win32/api/mftransform/nf-mftransform-imftransform-getstreamids
+		inputStreamID = 0;
+		outputStreamID = 0;
+		hr = S_OK;
+	}
+	if (FAILED(hr))
+		return hr;
+
+	if (FAILED(hr = ConfigureEncoder(transform, deviceManager, inputStreamID, outputStreamID)))
+		return hr;
+
 	// Shutdown
 	if (FAILED(hr = MFShutdown()))
 		return hr;
@@ -167,70 +230,6 @@ int main()
 	return 0;
 }
 
-//void runEncode()
-//{
-//
-//
-//
-//	// ------------------------------------------------------------------------
-//	// Initialize D3D11
-//	// ------------------------------------------------------------------------
-//
-//
-//
-//
-//	// ------------------------------------------------------------------------
-//	// Initialize hardware encoder MFT
-//	// ------------------------------------------------------------------------
-//
-//
-
-//	// Get stream IDs (expect 1 input and 1 output stream)
-//	{
-//		HRESULT hr = transform->GetStreamIDs(1, &inputStreamID, 1, &outputStreamID);
-//		if (hr == E_NOTIMPL)
-//		{
-//			inputStreamID = 0;
-//			outputStreamID = 0;
-//			hr = S_OK;
-//		}
-//		CHECK_HR(hr);
-//	}
-//
-//
-//	// ------------------------------------------------------------------------
-//	// Configure hardware encoder MFT
-//	// ------------------------------------------------------------------------
-//
-//	// Set D3D manager
-//	CHECK_HR(transform->ProcessMessage(MFT_MESSAGE_SET_D3D_MANAGER, reinterpret_cast<ULONG_PTR>(deviceManager.p)));
-//
-//	// Set output type
-//	CComPtr<IMFMediaType> outputType;
-//	CHECK_HR(MFCreateMediaType(&outputType));
-//
-//	CHECK_HR(outputType->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video));
-//	CHECK_HR(outputType->SetGUID(MF_MT_SUBTYPE, MFVideoFormat_H264));
-//	CHECK_HR(outputType->SetUINT32(MF_MT_AVG_BITRATE, 30000000));
-//	CHECK_HR(MFSetAttributeSize(outputType, MF_MT_FRAME_SIZE, ENCODE_WIDTH, ENCODE_HEIGHT));
-//	CHECK_HR(MFSetAttributeRatio(outputType, MF_MT_FRAME_RATE, 60, 1));
-//	CHECK_HR(outputType->SetUINT32(MF_MT_INTERLACE_MODE, 2));
-//	CHECK_HR(outputType->SetUINT32(MF_MT_ALL_SAMPLES_INDEPENDENT, TRUE));
-//
-//	CHECK_HR(transform->SetOutputType(outputStreamID, outputType, 0));
-//
-//	// Set input type
-//	CComPtr<IMFMediaType> inputType;
-//	CHECK_HR(transform->GetInputAvailableType(inputStreamID, 0, &inputType));
-//
-//	CHECK_HR(inputType->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video));
-//	CHECK_HR(inputType->SetGUID(MF_MT_SUBTYPE, MFVideoFormat_NV12));
-//	CHECK_HR(MFSetAttributeSize(inputType, MF_MT_FRAME_SIZE, ENCODE_WIDTH, ENCODE_HEIGHT));
-//	CHECK_HR(MFSetAttributeRatio(inputType, MF_MT_FRAME_RATE, 60, 1));
-//
-//	CHECK_HR(transform->SetInputType(inputStreamID, inputType, 0));
-//
-//
 //	// ------------------------------------------------------------------------
 //	// Create sample allocator
 //	// ------------------------------------------------------------------------
@@ -242,11 +241,11 @@ int main()
 //		CComPtr<IMFAttributes> allocAttrs;
 //		MFCreateAttributes(&allocAttrs, 2);
 //
-//		CHECK_HR(allocAttrs->SetUINT32(MF_SA_D3D11_BINDFLAGS, D3D11_BIND_RENDER_TARGET));
-//		CHECK_HR(allocAttrs->SetUINT32(MF_SA_D3D11_USAGE, D3D11_USAGE_DEFAULT));
+//		if (FAILED(hr = allocAttrs->SetUINT32(MF_SA_D3D11_BINDFLAGS, D3D11_BIND_RENDER_TARGET));
+//		if (FAILED(hr = allocAttrs->SetUINT32(MF_SA_D3D11_USAGE, D3D11_USAGE_DEFAULT));
 //
-//		CHECK_HR(allocator->SetDirectXManager(deviceManager));
-//		CHECK_HR(allocator->InitializeSampleAllocatorEx(1, 2, allocAttrs, inputType));
+//		if (FAILED(hr = allocator->SetDirectXManager(deviceManager));
+//		if (FAILED(hr = allocator->InitializeSampleAllocatorEx(1, 2, allocAttrs, inputType));
 //	}
 //
 //
@@ -254,27 +253,27 @@ int main()
 //	// Start encoding
 //	// ------------------------------------------------------------------------
 //
-//	CHECK_HR(transform->ProcessMessage(MFT_MESSAGE_COMMAND_FLUSH, NULL));
-//	CHECK_HR(transform->ProcessMessage(MFT_MESSAGE_NOTIFY_BEGIN_STREAMING, NULL));
-//	CHECK_HR(transform->ProcessMessage(MFT_MESSAGE_NOTIFY_START_OF_STREAM, NULL));
+//	if (FAILED(hr = transform->ProcessMessage(MFT_MESSAGE_COMMAND_FLUSH, NULL));
+//	if (FAILED(hr = transform->ProcessMessage(MFT_MESSAGE_NOTIFY_BEGIN_STREAMING, NULL));
+//	if (FAILED(hr = transform->ProcessMessage(MFT_MESSAGE_NOTIFY_START_OF_STREAM, NULL));
 //
 //	// Encode loop
 //	for (int i = 0; i < ENCODE_FRAMES; i++)
 //	{
 //		// Get next event
 //		CComPtr<IMFMediaEvent> event;
-//		CHECK_HR(eventGen->GetEvent(0, &event));
+//		if (FAILED(hr = eventGen->GetEvent(0, &event));
 //
 //		MediaEventType eventType;
-//		CHECK_HR(event->GetType(&eventType));
+//		if (FAILED(hr = event->GetType(&eventType));
 //
 //		switch (eventType)
 //		{
 //		case METransformNeedInput:
 //		{
 //			CComPtr<IMFSample> sample;
-//			CHECK_HR(allocator->AllocateSample(&sample));
-//			CHECK_HR(transform->ProcessInput(inputStreamID, sample, 0));
+//			if (FAILED(hr = allocator->AllocateSample(&sample));
+//			if (FAILED(hr = transform->ProcessInput(inputStreamID, sample, 0));
 //
 //			// Dereferencing the device once after feeding each frame "fixes" the leak.
 //			//device.p->Release();
@@ -288,15 +287,15 @@ int main()
 //			MFT_OUTPUT_DATA_BUFFER outputBuffer = {};
 //			outputBuffer.dwStreamID = outputStreamID;
 //
-//			CHECK_HR(transform->ProcessOutput(0, 1, &outputBuffer, &status));
+//			if (FAILED(hr = transform->ProcessOutput(0, 1, &outputBuffer, &status));
 //
 //			DWORD bufCount;
 //			DWORD bufLength;
-//			CHECK_HR(outputBuffer.pSample->GetBufferCount(&bufCount));
+//			if (FAILED(hr = outputBuffer.pSample->GetBufferCount(&bufCount));
 //
 //			CComPtr<IMFMediaBuffer> outBuffer;
-//			CHECK_HR(outputBuffer.pSample->GetBufferByIndex(0, &outBuffer));
-//			CHECK_HR(outBuffer->GetCurrentLength(&bufLength));
+//			if (FAILED(hr = outputBuffer.pSample->GetBufferByIndex(0, &outBuffer));
+//			if (FAILED(hr = outBuffer->GetCurrentLength(&bufLength));
 //
 //			printf("METransformHaveOutput buffers=%d, bytes=%d\n", bufCount, bufLength);
 //
@@ -314,15 +313,15 @@ int main()
 //	// Finish encoding
 //	// ------------------------------------------------------------------------
 //
-//	CHECK_HR(transform->ProcessMessage(MFT_MESSAGE_NOTIFY_END_OF_STREAM, NULL));
-//	CHECK_HR(transform->ProcessMessage(MFT_MESSAGE_NOTIFY_END_STREAMING, NULL));
-//	CHECK_HR(transform->ProcessMessage(MFT_MESSAGE_COMMAND_DRAIN, NULL));
+//	if (FAILED(hr = transform->ProcessMessage(MFT_MESSAGE_NOTIFY_END_OF_STREAM, NULL));
+//	if (FAILED(hr = transform->ProcessMessage(MFT_MESSAGE_NOTIFY_END_STREAMING, NULL));
+//	if (FAILED(hr = transform->ProcessMessage(MFT_MESSAGE_COMMAND_DRAIN, NULL));
 //
 //	// Shutdown
 //	printf("Finished encoding\n");
 //
 //	// I've tried all kinds of things...
-//	//CHECK_HR(transform->ProcessMessage(MFT_MESSAGE_SET_D3D_MANAGER, reinterpret_cast<ULONG_PTR>(nullptr)));
+//	//if (FAILED(hr = transform->ProcessMessage(MFT_MESSAGE_SET_D3D_MANAGER, reinterpret_cast<ULONG_PTR>(nullptr)));
 //
 //	//transform->SetInputType(inputStreamID, NULL, 0);
 //	//transform->SetOutputType(outputStreamID, NULL, 0);
@@ -331,5 +330,5 @@ int main()
 //
 //	//deviceManager->ResetDevice(NULL, resetToken);
 //
-//	CHECK_HR(MFShutdownObject(transform));
+//	if (FAILED(hr = MFShutdownObject(transform));
 //}
